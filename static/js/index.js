891c7d228a5db2348dcb91c5598c9e080488969f
Anway <anwaysomani@gmail.com>
2018-09-09 23:43:52 +0530

    Updated Index
    introduced designs for index(.html, .css, ,js)

--- static/js/index.js
+++ static/js/index.js
@@ -14,14 +14,3 @@ var initMap = function(){
 	});
 }
 
-
-	$(document).on("scroll", function(){
-		if
-      ($(document).scrollTop() > 86){
-		  $("#banner").addClass("shrink");
-		}
-		else
-		{
-			$("#banner").removeClass("shrink");
-		}
-	});
