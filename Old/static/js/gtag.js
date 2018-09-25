350a2b87e9ba57573097befb8e3b8dddc575e263
cyberityweb <annysomani@gmail.com>
2018-09-26 00:36:47 +0530

    Changed Website Design(v.2609)
    Changed everything....

--- Old/static/js/gtag.js
+++
@@ -1,9 +0,0 @@
-/* Google Analytics Tracking */
-
-window.dataLayer = window.dataLayer || [];
-
-function gtag(){dataLayer.push(arguments);}
-
-gtag('js', new Date());
-
-gtag('config', 'UA-113145627-1');
